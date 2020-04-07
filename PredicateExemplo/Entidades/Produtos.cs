0d91d6418b5ea05cd431c9f142cef86dd2b50654
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-04-07 08:39:43 -0300

    FINALIZADO

---
+++ PredicateExemplo/Entidades/Produtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PredicateExemplo.Entidades {
+    class Produtos {
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produtos(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString() {
+            return Nome + ", " + Preco.ToString("F2");
+        }
+    }
+}
--- PredicateExemplo/Program.cs
+++ PredicateExemplo/Program.cs
@@ -1,9 +1,38 @@
 ﻿using System;
+using PredicateExemplo.Entidades;
+using System.Collections.Generic;
+
 
 namespace PredicateExemplo {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //EM RESUMO, O PREDICATE É UM DELEGATE(UMA REFERENCIA PARA UMA FUNÇÃO)
+            //MAS ELE TEM QUE RETORNAR UMA FUNÇÃO DO TIPO BOLEANO 
+
+            List<Produtos> lista = new List<Produtos>();
+
+            lista.Add(new Produtos("TV", 900.00));
+            lista.Add(new Produtos("Mouse", 50.00));
+            lista.Add(new Produtos("Tablet", 350.50));
+            lista.Add(new Produtos("HD Case", 80.90));
+
+            //exemplo com a função lambda
+            lista.RemoveAll(p => p.Preco >= 100.0);
+            foreach (Produtos p in lista) {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            //exemplo com o predicate correto
+            //coloco a função dentro da condição do RemoveAll
+            lista.RemoveAll(ProdutoTest);
+            foreach (Produtos p2 in lista) {
+                Console.WriteLine(p2);
+            }
+        }
+        //função que retorna um resultado boleano se seguir as condições dentro do return!
+        public static bool ProdutoTest(Produtos p2) {
+            return p2.Preco >= 100.0;
         }
     }
 }
